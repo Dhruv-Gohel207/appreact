292824911f14f12b473554659a0214fbee08216d
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-11-27 12:46:09 +0530

    bcrypt and token done

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/?appname=MongoDB%20Comppass&directConnection=true&ssl=false';
+const mongoURI = 'mongodb://localhost:27017/inotebook';
 
 const connectToMongo = () => {
   console.log("Attempting to connect to MongoDB...");
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/User')
+const User = require('../models/User'); 
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = "Something@secret"
 
 
-//create user using post "/api/auth".   doesn't require Auth
-router.post('/', (req, res)=>{
- 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+// Create user using POST "/api/auth/createuser". No authentication required
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 })
 
-})
-module.exports = router
+
+], async (req, res) => {
+    const errors = validationResult(req);
+        
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "A user with this email already exists"  });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const secpass = await bcrypt.hash(req.body.password, salt);
+        
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: secpass
+        });
+        await user.save();
+        const data = {
+            user:{
+                id:user.id 
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET)            
+        // res.json({ message: "User created successfully", user });
+        res.json({authToken})
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+module.exports = router;
